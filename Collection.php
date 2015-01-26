e8fc1b64d2a9264931547e2f6f460f1c377fcd38
Ryan Kinal <ryan.kinal@gmail.com>
2015-01-26 13:17:24 -0500

    Fixed namespacing

--- Collection.php
+++ Collection.php
@@ -1,6 +1,4 @@
 <?php
-include_once('Database.php');
-
 namespace stORM;
 
 abstract class Collection
--- DBRecord.php
+++ DBRecord.php
@@ -1,8 +1,6 @@
 <?php
 namespace stORM;
 
-include_once('Database.php');
-
 abstract class DBRecord
 {
 	protected $dirty;
